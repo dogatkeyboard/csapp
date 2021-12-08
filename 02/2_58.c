31bf168315d349df3cc73c9a278d1877d66b346d
dogatkeyboard <nordbored@protonmail.com>
2021-12-08 02:55:04 +0000

    added a note about endianess

--- 02/2_58.c
+++ 02/2_58.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "../common/csapp.h"
 
+/**
+ * Write a procedure is_little_endian that will return 1 when compiled and run
+ * on a little-endian machine, and will return 0 when compiled and run on a
+ * big-endian machine. This program should run on any machine, regardless of
+ * its word size.
+ */
+
 int main() {
     if (is_little_endian()) {
         printf("Little Endian environment found!\n");
@@ -10,3 +17,10 @@ int main() {
     }
     return 0;
 }
+
+/**
+ * Had trouble finding a big-endian machine to test this on, since my windows
+ * and linux systems are little-endian. I would love to setup a big-endian
+ * system to make sure this works as I expect it to, but I don't see any
+ * practical options for the time being that would allow it.
+ */
